7d80c146fd8495898bd06d358341cdd9fc2d6c9f
kayjee <Kagiso.killagram@gmail.com>
2023-04-06 11:28:16 +0200

    Added algo fizz buzz code

---
+++ fizzbuzz.js
@@ -0,0 +1,28 @@
+module.exports = function () {
+  const array = [];
+  for (let i = 0; i < 100 ; i++){
+    if ((i+1) % 3 == 0) {
+      array [i] = 'Fizz';
+    }
+
+    if ((i+1) % 5 == 0) {
+      array [i] = 'Buzz';
+    }
+
+    if ((i+1) % 3 == 0 && (i+1) % 5 == 0) {
+      array [i] = 'FizzBuzz';
+    }
+
+    if ((i+1) % 3 != 0 && (i+1) % 5 != 0){
+      array[i] = i+1;
+    }
+  }
+
+  for(let i = 1 ; i <= 100 ; i++){
+    console.log(array[i]);
+  }
+  if(array[2]==='Fizz'){
+    console.log("true");
+  }
+  return array;
+}
